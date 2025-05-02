59180cedc1b1b4ba85f8215c45075f2833faf454
Rangarajan Rajendiran <rrangarajan.85@gmail.com>
2025-05-02 23:43:02 +0530

    00012 - k8s - env variables

--- k8s/run.sh
+++ k8s/run.sh
@@ -4,3 +4,5 @@ kubectl delete service/myservice-nginx
 kubectl create -f deployment-nginx-definition.yaml
 kubectl create -f deployment-busybox-definition.yaml
 kubectl create -f service-nginx-definition.yaml
+sleep 3
+kubectl get pods

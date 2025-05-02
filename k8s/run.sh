387958bbf8c1b35451861d9a7ffad9865d2e32a0
Rangarajan Rajendiran <rrangarajan.85@gmail.com>
2025-05-02 21:27:04 +0530

    00011 - k8s - running nginx on custom port

---
+++ k8s/run.sh
@@ -0,0 +1,6 @@
+kubectl delete deployment.apps/mydeployment-busybox
+kubectl delete deployment.apps/mydeployment-nginx
+kubectl delete service/myservice-nginx
+kubectl create -f deployment-nginx-definition.yaml
+kubectl create -f deployment-busybox-definition.yaml
+kubectl create -f service-nginx-definition.yaml

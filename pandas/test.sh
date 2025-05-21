5ec827f839792ba7e626263789f46f470ba4f511
Rangarajan Rajendiran <rrangarajan.85@gmail.com>
2025-05-21 11:31:52 +0530

    00017 - pandas - added

---
+++ pandas/test.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get install -y texlive-xetex
+sudo apt-get install -y texlive-fonts-recommended
+sudo apt-get install -y texlive-plain-generic
+sudo apt-get install -y texlive-latex-extra
+sudo apt install -y pandoc
+sudo apt install -y texlive
